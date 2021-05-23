467ea52a2d0f86843ec852b4298f5f0f85a706a7
alamshafil <36016500+alamshafil@users.noreply.github.com>
2021-05-23 14:19:41 -0400

    Add UIKit and basic routing

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,3 +1,13 @@
-function test(e) {
-    document.write(e);
+window.addEventListener("load", () => {
+    // Show home screen, but hide mining screen when page is loaded
+    getHome().style.display = "block";
+    getMine().style.display = "none";
+}, false);
+
+function getHome() {
+    return document.getElementById('home-screen');
+}
+
+function getMine() {
+    return document.getElementById('mine-screen');
 }
