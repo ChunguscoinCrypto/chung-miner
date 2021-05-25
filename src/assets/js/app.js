eb85b2231374a4adc87614de5fd6f5efd46dbb4e
alamshafil <36016500+alamshafil@users.noreply.github.com>
2021-05-25 17:18:07 -0400

    Check address box if blank

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,3 +1,8 @@
+const { dialog } = require('electron').remote;
+const shell = require('electron').shell;
+const path = require('path');
+
+
 window.addEventListener("load", () => {
     // Show home screen, but hide mining screen when page is loaded
     getHome().style.display = "block";
@@ -10,4 +15,37 @@ function getHome() {
 
 function getMine() {
     return document.getElementById('mine-screen');
+}
+
+function showMiner() {
+    address = document.getElementById('chung-address').value;
+    if (address.length == 0) {
+        showAddrError();
+        return;
+    }
+
+    getHome().style.display = "none";
+    getMine().style.display = "block";
+
+    getMine().innerHTML += `\n Welcome Chung User, ${address}!`;
+}
+
+function showAddrError() { 
+    function getWallet() { shell.openExternal("http://wallet.chunguscoin.net") };
+    dialog.showMessageBox({        
+        type: 'info',
+        buttons: ["OK", "Get Wallet"], 
+        defaultId: 0,
+        icon: 'info',
+        title: 'Chung Miner',
+        message: 'No Chunguscoin address was entered.',
+        cancelId: 0,
+        noLink: true,
+        normalizeAccessKeys: false,
+    }).then(box => {
+        if(box.response == 1) 
+            getWallet()
+    }).catch(err => {
+        alert(err)
+    });    
 }
--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ const createWindow = () => {
     autoHideMenuBar: true,
     webPreferences: {
       nodeIntegration: true,
+      enableRemoteModule: true,
       contextIsolation: false,
     }
   });
